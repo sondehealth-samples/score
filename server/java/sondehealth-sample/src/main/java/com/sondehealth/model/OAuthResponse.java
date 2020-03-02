62137cf2325ccc4461405916fb4e5783b050065d
Ankit Kalantri <Ankit.Kalantri@gslab.com>
2020-03-02 10:30:37 +0530

    added sample server

---
+++ server/java/sondehealth-sample/src/main/java/com/sondehealth/model/OAuthResponse.java
@@ -0,0 +1,20 @@
+package com.sondehealth.model;
+
+public class OAuthResponse {
+
+	private Object response;
+	private Integer StatusCode;
+	public Object getResponse() {
+		return response;
+	}
+	public void setResponse(Object response) {
+		this.response = response;
+	}
+	public Integer getStatusCode() {
+		return StatusCode;
+	}
+	public void setStatusCode(Integer statusCode) {
+		StatusCode = statusCode;
+	}
+	
+}
---
+++ server/java/sondehealth-sample/src/main/java/com/sondehealth/model/SubjectCreationRequest.java
@@ -0,0 +1,30 @@
+package com.sondehealth.model;
+
+import java.util.List;
+
+public class SubjectCreationRequest {
+
+	private Integer yearOfBirth;
+	private String gender;
+	private List<String> languages;
+	public Integer getYearOfBirth() {
+		return yearOfBirth;
+	}
+	public void setYearOfBirth(Integer yearOfBirth) {
+		this.yearOfBirth = yearOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public List<String> getLanguages() {
+		return languages;
+	}
+	public void setLanguages(List<String> languages) {
+		this.languages = languages;
+	}
+	
+	
+}
