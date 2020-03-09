eea8816147488b75193f98e9531156fbec06d2d8
Ajay <ajay.choudhary@gslab.com>
2020-03-09 15:23:08 +0530

    Added user registration flow

--- client/android/app/src/main/java/com/sonde/sample/utils/Configuration.java
+++ client/android/app/src/main/java/com/sonde/sample/utils/Configuration.java
@@ -2,5 +2,6 @@ package com.sonde.sample.utils;
 
 public class Configuration {
     public static final String BASE_URL = "##########"; // Replace with provided url by Sonde
-    public static final String ACCESS_TOKEN_URL = "############";// replace with your own server end point
+    public static final String ACCESS_TOKEN_URL = "localhost:8080/oauth2/token";// replace with your own server end point
+    public static final String REGISTER_USER = "localhost:8080/platform/v1/users";// replace with your own server end point
 }
